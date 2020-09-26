e9a730deba8d1a348bcbc86b7b089ce8aaca6583
Tom Austin <taustin@bias2build.com>
2020-09-26 01:18:32 -0700

    Refactoring code to use a mixin

--- locking-miner.js
+++ locking-miner.js
@@ -1,16 +1,14 @@
 "use strict";
 
 const Miner = require('./miner.js');
-const LockingClient = require('./locking-client.js');
+
+const lockingMixin = require('./locking-mixin.js');
 
 module.exports = class LockingMiner extends Miner {
   constructor(...args) {
     super(...args);
 
-    // Adding methods from the LockingClient class.
-    this._plt = LockingClient.prototype.postLockingTransaction;
-    this.lockedGold = LockingClient.prototype.lockedGold;
-    this.showAllBalances = LockingClient.prototype.showAllBalances;
+    Object.assign(this, lockingMixin);
   }
 
   /**
@@ -21,16 +19,6 @@ module.exports = class LockingMiner extends Miner {
   get availableGold() {
     // FIXME: Repeated code from LockingClient version -- need to refactor away.
     return super.availableGold - this.lockedGold();
-  }
-
-  /**
-   * When a miner posts a transaction, it must also add it to its current list of transactions.
-   * 
-   * @param  {...any} args - Arguments needed for Client.postTransaction.
-   */
-  postLockingTransaction(...args) {
-    let tx = this._plt(...args);
-    this.addTransaction(tx);
   }
 
 };
